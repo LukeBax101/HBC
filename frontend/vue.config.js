362a8654e8e30b65ad95a62d42bd234b1e393028
root <root@vmi348166.contaboserver.net>
2020-03-08 19:08:36 +0100

    PWA working

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,3 +1,38 @@
 module.exports = {
     publicPath: '/hbc',
+    pwa: {
+	name: 'Holiday Bible Club',
+	themeColor: '#FF8857',
+	appleMobileWebAppCapable: 'yes',
+	appleMobileWebAppStatusBarStyle: 'black',
+	iconPaths: {
+	    favicon32: 'img/icons/hbc-favicon-32x32.png',
+	    favicon16: 'img/icons/hbc-favicon-16x16.png',
+	    appleTouchIcon: 'img/icons/hbc-apple-touch-icon.png'
+        },
+	manifestOptions: {
+	    short_name: 'HBC',
+	    background_color: '#E8A643',
+	    icons: [
+                {
+                    'src': 'img/icons/hbc-android-chrome-192x192.png',
+                    'sizes': '192x192',
+                    'type': 'image/png',
+                },
+                {
+                    'src': 'img/icons/hbc-android-chrome-512x512.png',
+                    'sizes': '512x512',
+                    'type': 'image/png',
+                }
+            ]
+	}
+    },
+    chainWebpack: (config) => {
+    config
+      .plugin('html')
+      .tap((args) => {
+        args[0].title = 'Holiday Bible Club';
+        return args;
+      });
+  }
 };
