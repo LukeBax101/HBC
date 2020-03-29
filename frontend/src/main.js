f6776c61e84ccfd0f8d3d6c4b67c2833c9bb2bfb
Luke Baxter <lbb29@cam.ac.uk>
2020-03-29 03:40:18 +0100

    SocketIO Fix Attempt 3

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import VueSocketIOExt from 'vue-socket.io-extended';
 import socketio from 'socket.io-client';
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue';
-import { API_URL, SOCKET_PATH } from './properties';
+import { SOCKET_URL, SOCKET_PATH } from './properties';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
@@ -13,7 +13,7 @@ import 'bootstrap-vue/dist/bootstrap-vue.css';
 Vue.config.productionTip = false;
 
 
-export const socket = socketio(API_URL, {
+export const socket = socketio(SOCKET_URL, {
   path: SOCKET_PATH,
   transports: ['websocket'],
 });
--- frontend/src/properties.js
+++ frontend/src/properties.js
@@ -1,2 +1,3 @@
 export const API_URL = process.env.NODE_ENV === 'production' ? `${window.location.origin}/hbc` : 'http://localhost:4000';
+export const SOCKET_URL = process.env.NODE_ENV === 'production' ? `${window.location.origin}` : 'http://localhost:4000';
 export const SOCKET_PATH = process.env.NODE_ENV === 'production' ? '/hbc/socket.io' : '/socket.io';
